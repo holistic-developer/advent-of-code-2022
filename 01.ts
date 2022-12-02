9ade64dbf19e598f5ce8ab65e6643cdbbf1ffbc6
Andreas Taranetz <andreas.taranetz@dynatrace.com>
2022-12-02 06:53:44 +0100

    Day 1

--- 01.ts
+++ 01.ts
@@ -1,13 +1,19 @@
-import * as fs from 'fs'
+import * as fs from 'fs';
 
-let rawInput = fs.readFileSync('01-input.txt')
+let rawInput = fs.readFileSync('01-input.txt');
 const input = rawInput
   .toString()
-  .split('\n')
-  .map((value) => parseInt(value))
+  .split('\n\n')
+  .map((elf) => elf.split('\n')
+    .map((value) => parseInt(value))
+    .reduce((a, b) => a + b));
 
 // stage 1
-
+console.log(Math.max(...input));
 
 
 // stage 2
+const top3 = input.sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((a, b) => a + b);
+console.log(top3);
